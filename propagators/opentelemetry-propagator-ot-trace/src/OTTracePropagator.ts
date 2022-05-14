6fdb41f5375db5cc9a3c6f51fdaa14ccaaab49da
Hugo Cortes <contact@hugocortes.dev>
2022-05-14 15:44:18 -0700

    fix: remove traceId substr(16)

--- propagators/opentelemetry-propagator-ot-trace/src/OTTracePropagator.ts
+++ propagators/opentelemetry-propagator-ot-trace/src/OTTracePropagator.ts
@@ -67,7 +67,7 @@ export class OTTracePropagator implements TextMapPropagator {
     const spanContext = trace.getSpan(context)?.spanContext();
     if (!spanContext || !isSpanContextValid(spanContext)) return;
 
-    setter.set(carrier, OT_TRACE_ID_HEADER, spanContext.traceId.substr(16));
+    setter.set(carrier, OT_TRACE_ID_HEADER, spanContext.traceId);
     setter.set(carrier, OT_SPAN_ID_HEADER, spanContext.spanId);
     setter.set(
       carrier,
